0f8819d29dff1ad082380b6291b8510dda7f1b0a
Haim Martinez <jhaim04@gmail.com>
2021-11-16 12:27:56 -0500

    Acomodando etiquetas

--- Test.py
+++ Test.py
@@ -10,14 +10,16 @@ from tkinter import *
 ventana = tkinter.Tk()
 #ventana.attributes("-alpha", 0.8) #con este comando hago que la ventana sea transparente y el numero indicara el nivel de transparencia.
 ventana.title("Incio de Sesion") #titulo de la ventana
-ventana.geometry("300x300+540+200") #damos tamano a la ventana y la ubicamos en la pantalla
+ventana.geometry("350x400+540+200") #damos tamano a la ventana y la ubicamos en la pantalla
 ventana.resizable(0,0) #evitamos que el usuario modifique el tamano de la ventana
 
 
 #empiezo a declarar un label:
-etiqueta = tkinter.Label(ventana, text = "hola").grid(column=1, row=4)  
-etiqueta2= tkinter.Label(ventana, text = "saludos").grid(column=0, row=0)
-
+etiqueta1 = tkinter.Label(ventana, text = "Etiqueta1").grid(column=0, row=0)  
+etiqueta2 = tkinter.Label(ventana, text = "Etiqueta2").grid(column=1, row=1)
+etiqueta3 = tkinter.Label(ventana, text = "Etiqueta3").grid(column=2, row=2)
+etiqueta4 = tkinter.Label(ventana, text = "Etiqueta4").grid(column=3, row=3)
+etiqueta5 = tkinter.Label(ventana, text = "Etiqueta5", bg = "gray").grid(column=4, row=4)
 
 
 
