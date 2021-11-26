c89f18daf3c5303acb4ae2867a1792aafa35784d
Haim Martinez <jhaim04@gmail.com>
2021-11-26 17:48:45 -0500

    Dandole color de fondo y formato a la ventana de logueo

--- Test.py
+++ Test.py
@@ -10,26 +10,27 @@ from tkinter import * #Importamos todos los widgets que tkinter tiene para gener
 ventana = tkinter.Tk()
 #ventana.attributes("-alpha", 0.8) #con este comando hago que la ventana sea transparente y el numero indicara el nivel de transparencia.
 ventana.title("Haim Martinez - jhaim04@gmail.com") #titulo de la ventana
-ventana.geometry("300x300+400+200") #damos tamano a la ventana y la ubicamos en la pantalla
+ventana.geometry("300x250+400+200") #damos tamano a la ventana y la ubicamos en la pantalla
 ventana.resizable(0,0) #evitamos que el usuario modifique el tamano de la ventana
 
 #Declaracion del Label Frame1
-labelframe1 = LabelFrame(ventana, text="    Inicio de Sesion    ", padx=50, pady=40) #declaramos el labelframe1
-labelframe1.pack(padx=20, pady=20) #le indicamos que muestre en pantalla el labelframe1
+labelframe1 = LabelFrame(ventana, text="Inicio de Sesion", padx=50, pady=40, bg = "azure2") #declaramos el labelframe1
+labelframe1.pack(padx=10, pady=10) #le indicamos que muestre en pantalla el labelframe1
 
 #Declaracion de los label de usuario y contraseña
-usuario=tkinter.Label(labelframe1, text= "Usuario:")
-usuario.pack()
+usuario=tkinter.Label(labelframe1, text= "Usuario:", font = "Helvetica 14 bold", bg = "azure2")
+usuario.pack(anchor=NW)
 user = tkinter.Entry(labelframe1, bd = 3)
 user.pack()
-contrasena=tkinter.Label(labelframe1, text = "Contrasena:")
-contrasena.pack()
-password = tkinter.Entry(labelframe1, bd = 3)
+user.focus()
+contrasena=tkinter.Label(labelframe1, text = "Contraseña:", font = "Helvetiva 14 bold", bg = "azure2")
+contrasena.pack(anchor =  NW)
+password = tkinter.Entry(labelframe1, show = "*", bd = 3)
 password.pack()
 
 
 #Declaracion del los botones
-b=Button(labelframe1, text="Ingresar", padx=20, pady=10)
+b=Button(labelframe1, text="Ingresar", padx=20, pady=30)
 b.pack()
 
 
